1af9f2201cf8338580a4ecad468ac5b2ab39fea2
Matthew Brett <matthew.brett@gmail.com>
2023-11-27 14:42:14 +0000

    Add project code .py file

---
+++ projtools/projcode.py
@@ -0,0 +1,22 @@
+""" Project code
+
+This is just an example.
+
+You should document your code, as in the example below, using the Numpy
+docstring format (Google it).
+"""
+
+def my_func(a, b):
+    """ An example function, returning `a` * 10 + b
+
+    Parameters
+    ----------
+    a : number
+    b : number
+
+    Returns
+    -------
+    res : number
+        Result of ``a * 10 + b``.
+    """
+    return a * 10 + b
