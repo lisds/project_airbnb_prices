2d17ac3f52fffdf64fe222499044a39d104c3212
SvetlanaVP <svetlanavpopov@gmail.com>
2023-12-01 15:51:53 +0000

    set index data cleaning

--- projtools/cleaningdata.py
+++ projtools/cleaningdata.py
@@ -1,1 +1,10 @@
-print('hello')
+import numpy as np
+
+rng = np.random.default_rng()
+
+import pandas as pd
+
+pd.set_option('mode.copy_on_write', True)
+
+data = pd.read_csv('data/clean_df.csv')
+data
