7541a943c6ea7c306fd627530afd90d95cacc4fe
SvetlanaVP <svetlanavpopov@gmail.com>
2023-12-01 16:12:13 +0000

    fixed .py file

--- projtools/cleaningdata.py
+++ projtools/cleaningdata.py
@@ -6,5 +6,9 @@ import pandas as pd
 
 pd.set_option('mode.copy_on_write', True)
 
-data = pd.read_csv('data/clean_df.csv')
-data
+script_dir = os.path.dirname('clean_df.csv')
+full_path = os.path.join(script_dir, '../data/clean_df.csv')
+data = pd.read_csv(full_path)
+
+data_indexed = data.set_index('id')
+data_indexed
