06b14e00413f237c8841a3f76784a05e4be6ab79
Oscar <oscar.robinson-alam@lis.ac.uk>
2023-12-12 14:07:55 +0000

    Some stuff on the cleaning

--- projtools/cleaning.py
+++ projtools/cleaning.py
@@ -15,16 +15,24 @@ def data_setup():
     pd.DataFrame
         Cleaned and processed DataFrame.
     """
-
+    
+    #Setting path to data
     data_path = Path('../data/proj_data.csv')
 
+    #Creating Pandas df
     raw_data = pd.read_csv(data_path)
 
+    #Setting the index
     data = raw_data.set_index('id')
+    
+    #Renaming columns
     data = data.rename(columns={
         'neighborhood_overview': 'neighbourhood_description',
         'neighbourhood_cleansed': 'neighbourhood_location'
         
     })
 
+    #Dropping unecessary columns
+    data = data.drop(columns=['listing_url','host_picture_url', 'host_url'])
+   
     return data
